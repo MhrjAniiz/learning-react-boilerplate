07dce850c67e578af287ad1b1aa0b2db573d457e
MhrjAniiz <anishmaharjan17@gmail.com>
2020-12-01 06:15:10 +0545

    Nav bar added

---
+++ app/components/Navbar/Nav.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Nav = styled.div`
+  width: 100%;
+  height: 60px;
+  background-color: black;
+  color: white;
+  display: flex;
+  align-items: center;
+`;
+
+export default Nav;
